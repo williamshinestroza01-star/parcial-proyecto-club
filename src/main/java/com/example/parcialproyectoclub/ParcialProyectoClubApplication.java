32253c0d88bff896aa30322beb831f42a711fb55
williamshinestroza01-star <williams.hinestroza01@unicatolica.edu.co>
2026-04-12 19:21:50 -0500

    Add files via upload

---
+++ src/main/java/com/example/parcialproyectoclub/ParcialProyectoClubApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcialproyectoclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialProyectoClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcialProyectoClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parcialproyectoclub/ParcialProyectoClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialproyectoclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialProyectoClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
